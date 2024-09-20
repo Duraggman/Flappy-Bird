ccc5e847919611100fa1c40793338eeac36e5960
Duraggman <tyrellgw10@gmail.com>
2024-09-21 00:26:59 +0100

    commit 3: added pipe

--- src/Main.hx
+++ src/Main.hx
@@ -1,22 +1,36 @@
+import h2d.Bitmap;
+
 class Main extends hxd.App {
     var bmp : h2d.Bitmap;
     override function init() {
+        // initialising hxd.Res
         hxd.Res.initEmbed();
-        var bg = hxd.Res.fb_BG_jpeg.toTile();
+        
+        // creating the background
+        var bg = hxd.Res.fb_BG.toTile();
         bg.scaleToSize(1366, 766);
-        // 
+
         // create a Bitmap object, which will display the tile
         // and will be added to our 2D scene (s2d)
-        var bmp = new h2d.Bitmap(bg, s2d);
-        // modify the display position of the Bitmap sprite
-        bmp.x = 0;
-        bmp.y = 0;
+        var bgbmp = new h2d.Bitmap(bg, s2d);
 
-        //Creating the bir
+        // modify the display position of the Bitmap sprite
+        bgbmp.x = 0;
+        bgbmp.y = 0;
+
+        //Creating the bird
         var birdTile = hxd.Res.bird_mid_nbg.toTile();
         var birdBmp = new h2d.Bitmap(birdTile, s2d);
+        
+        // setting bird pos
         birdBmp.x = 300;
         birdBmp.y = 400;
+
+        //creating pipes
+        var pipeTile = hxd.Res.pipe.toTile();
+        var pipeBmp = new Bitmap(pipeTile, s2d);
+        pipeBmp.x = 600;
+        pipeBmp.y = 500;
     }
 
     // on each frame
