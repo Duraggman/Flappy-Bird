16f2bef658242396c98256cdbd081148defd4bdf
Duraggman <tyrellgw10@gmail.com>
2024-09-21 00:07:49 +0100

    commit 2: added bird tile

--- src/Main.hx
+++ src/Main.hx
@@ -4,13 +4,19 @@ class Main extends hxd.App {
         hxd.Res.initEmbed();
         var bg = hxd.Res.fb_BG_jpeg.toTile();
         bg.scaleToSize(1366, 766);
-        // create bitmap used for background
+        // 
         // create a Bitmap object, which will display the tile
         // and will be added to our 2D scene (s2d)
         var bmp = new h2d.Bitmap(bg, s2d);
         // modify the display position of the Bitmap sprite
         bmp.x = 0;
         bmp.y = 0;
+
+        //Creating the bir
+        var birdTile = hxd.Res.bird_mid_nbg.toTile();
+        var birdBmp = new h2d.Bitmap(birdTile, s2d);
+        birdBmp.x = 300;
+        birdBmp.y = 400;
     }
 
     // on each frame
