8d030b272600a17dbff7993a30e8a7fa068c5af1
Duraggman <tyrellgw10@gmail.com>
2024-09-21 14:13:03 +0100

    commit 4: Added gravity and pipe movement.

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,11 @@
-import h2d.Bitmap;
+import hxd.Key;
+import sys.thread.EventLoop.EventHandler;
 
 class Main extends hxd.App {
-    var bmp : h2d.Bitmap;
+    var birdBmp : h2d.Bitmap;
+    var pipeTBmp : h2d.Bitmap;
+    var pipeBBmp : h2d.Bitmap;
+
     override function init() {
         // initialising hxd.Res
         hxd.Res.initEmbed();
@@ -13,34 +17,41 @@ class Main extends hxd.App {
         // create a Bitmap object, which will display the tile
         // and will be added to our 2D scene (s2d)
         var bgbmp = new h2d.Bitmap(bg, s2d);
-
         // modify the display position of the Bitmap sprite
         bgbmp.x = 0;
         bgbmp.y = 0;
 
         //Creating the bird
         var birdTile = hxd.Res.bird_mid_nbg.toTile();
-        var birdBmp = new h2d.Bitmap(birdTile, s2d);
+        birdBmp = new h2d.Bitmap(birdTile, s2d);
         
         // setting bird pos
-        birdBmp.x = 300;
-        birdBmp.y = 400;
+        birdBmp.setPosition(300,400);
 
         //creating pipes
-        var pipeTile = hxd.Res.pipeBot.toTile();
-        var pipeBmp = new Bitmap(pipeTile, s2d);
-        pipeBmp.x = 600;
-        pipeBmp.y = 500;
+        var pipeBotTile = hxd.Res.pipeBot.toTile();
+        pipeBBmp = new h2d.Bitmap(pipeBotTile, s2d);
+        pipeBBmp.x = 600;
+        pipeBBmp.y = 500;
 
-        var pipeTile = hxd.Res.pipeTop.toTile();
-        var pipeBmp = new Bitmap(pipeTile, s2d);
-        pipeBmp.x = 600;
-        pipeBmp.y = 0;
+        var pipeTopTile = hxd.Res.pipeTop.toTile();
+        pipeTBmp = new h2d.Bitmap(pipeTopTile, s2d);
+        pipeTBmp.x = 600;
+        pipeTBmp.y = 0;
     }
-
     // on each frame
     override function update(dt:Float) {
         // sim gravity
+        if (!hxd.Key.isPressed(Key.SPACE)){
+            birdBmp.setPosition(birdBmp.x, birdBmp.y+0.5);
+        }
+        else{
+            birdBmp.setPosition(birdBmp.x, birdBmp.y-10);
+        }
+
+        // Move pipes to the left
+        pipeBBmp.setPosition(pipeBBmp.x-1,pipeBBmp.y);
+        pipeTBmp.setPosition(pipeTBmp.x-1, pipeTBmp.y);
     }
 
     static function main() {
